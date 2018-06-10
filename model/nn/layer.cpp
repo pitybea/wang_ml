64eff979645d6a49f5aa498c7003e6aac76e0179
Zhipeng Wang <wangzhp85@gmail.com>
2018-06-10 22:35:35 +0900

    difficult

--- model/nn/layer.cpp
+++ model/nn/layer.cpp
@@ -2,21 +2,22 @@
 
 #include "./layer.h"
 
-layer::layer(int input_d, int output_d) :
+layer::layer(int input_d, int output_d, int rank = 0) :
     input_dim(input_d), output_dim(output_d),
-    parameters(new layerPara<floatType>(input_d, output_d, 1.0f / input_d)) {
+    parameters(layerPara<floatType>(input_d, output_d, rank, 0.0f)) {
 }
 
 void layer::linearFunc(vector<floatType>* result,
                        const vector<floatType>& input) {
   for (int i = 0; i < output_dim; i++) {
-    (*result)[i] = parameters->b[i];
+    (*result)[i] = parameters.b[i];
     for (int j = 0; j < input_dim; j++) {
-      (*result)[i] += parameters->w[i][j] * input[j];
+      (*result)[i] += parameters.w[i][j] * input[j];
     }
   }
 }
 
-layer::~layer() {
-  delete parameters;
+layerFM::layerFM(int input_d, int class_num_, int rank_) :
+    layer(input_d, class_num_ - 1, rank_), class_num(class_num_),
+    rank(rank_) {
 }
--- model/nn/layer.h
+++ model/nn/layer.h
@@ -10,25 +10,42 @@ using std::vector;
 
 template<class T>
 struct layerPara {
-  vector<vector<T>> w;
   vector<T> b;
-  layerPara(int input_d, int output_d, T val) :
+  vector<vector<T>> w;
+  vector<vector<vector<T>>> v;
+  layerPara(int input_d, int output_d, int rank, T val) :
     w(vector<vector<T>>(output_d, vector<T>(input_d, val))),
-    b(vector<T>(output_d, val)) {}
+    b(vector<T>(output_d, val)) {
+      if (rank > 0) {
+        v = vector<vector<vector<T>>>(output_d, vector<vector<T>>
+                                      (input_d, vector<T>(rank, val)));
+      }
+    }
 };
 
 class layer {
  public:
-  layer(int, int);
-  void linearFunc(vector<floatType>*,
-                  const vector<floatType>&);
-  ~layer();
+  layer(int, int, int);
+  void linearFunc(vector<floatType>*, const vector<floatType>&);
+  virtual void forward(vector<floatType>*, const vector<floatType>&);
+  virtual void backward(vector<floatType>*, const vector<floatType>&,
+                const vector<floatType>&, const vector<floatType>&);
   
  private:
   int input_dim;
   int output_dim;
-  layerPara<floatType>* parameters;
+  layerPara<floatType> parameters;
   
+};
+
+class layerFM : public layer {
+ public:
+  layerFM(int, int, int);
+  void initV(floatType);
+  
+ private:
+  int class_num;
+  int rank;
 };
 
 #endif  // MODEL_NN_LAYER_H_
